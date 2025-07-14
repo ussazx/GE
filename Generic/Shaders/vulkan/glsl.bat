6db8b1325f396804ebc1b24fedb91fa3b09aeff6
asus <aa@ggg.com>
2025-07-14 11:27:26 +0800

    .

--- Generic/Shaders/vulkan/glsl.bat
+++ Generic/Shaders/vulkan/glsl.bat
@@ -4,5 +4,5 @@
 ..\..\..\ThirdParty\vulkan\Bin32\glslangValidator.exe -V -o ..\..\..\Binaries\resources\shaders\vulkan\ui_id.vsc ui_id.vert
 ..\..\..\ThirdParty\vulkan\Bin32\glslangValidator.exe -V -o ..\..\..\Binaries\resources\shaders\vulkan\ui_id.psc ui_id.frag
 
-..\..\..\ThirdParty\vulkan\Bin32\glslangValidator.exe -V -o ..\..\..\Binaries\resources\shaders\vulkan\object3d_vs.vsc object3d.vert
-..\..\..\ThirdParty\vulkan\Bin32\glslangValidator.exe -V -o ..\..\..\Binaries\resources\shaders\vulkan\object3d_ps.psc object3d.frag
+#..\..\..\ThirdParty\vulkan\Bin32\glslangValidator.exe -V -o ..\..\..\Binaries\resources\shaders\vulkan\object3d_vs.vsc object3d.vert
+#..\..\..\ThirdParty\vulkan\Bin32\glslangValidator.exe -V -o ..\..\..\Binaries\resources\shaders\vulkan\object3d_ps.psc object3d.frag
